6fcdd7c2193cf8699bed952250e3ed19c043cd63
m5s3 <devmyoungsoo2@gmail.com>
2023-07-03 20:29:44 +0900

    엔티티 클래스 개발 완료

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -24,11 +26,16 @@ public class Category {
     )
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    //==연관관계 매서드==/
+    public void setChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
